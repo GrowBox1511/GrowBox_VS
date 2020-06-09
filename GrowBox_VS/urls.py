9c758f3ca8b2d36448cdeaf2190548e66511ca1d
Иван Бондаренко <tybond@mail.ru>
2020-06-09 23:27:48 +0300

    idk

--- GrowBox_VS/urls.py
+++ GrowBox_VS/urls.py
@@ -10,6 +10,7 @@ from app import forms, views
 
 
 urlpatterns = [
+     #xddddddddddd
     path('', views.home, name='home'),
     path('contact/', views.contact, name='contact'),
     path('about/', views.about, name='about'),
